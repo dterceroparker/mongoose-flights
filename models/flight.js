50c5746fbac1f43a9f19238634584a874e072338
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-16 18:53:46 -0400

    add a ticket for flight and update show.ejs

--- models/flight.js
+++ models/flight.js
@@ -3,9 +3,6 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
-
-
-
 const ticketSchema = new Schema({
   seat: {
     type: String,
@@ -18,8 +15,6 @@ const ticketSchema = new Schema({
 }, {
   timestamps: true
 })
-
-
 
 const flightSchema = new Schema({
   airline: {
