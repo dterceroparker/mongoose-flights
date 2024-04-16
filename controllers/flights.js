4746972f538e1fc276f48607aadc4cbfc1d57ca4
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-15 21:50:40 -0400

    create & show routes in controller functions & views completion

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,13 +1,62 @@
 import { Flight } from '../models/flight.js'
 
+function index(req, res) {
+   // use flight model to search for all flights
+  Flight.find({})
+  .then(flights => {
+    // render a view (flights/index) and pass flights and title 
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Flight.findById(req.params.flightId)
+  .then(flight => {
+    res.render('flights/show', {
+      flight: flight,
+      title: 'Flight Detail'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 function newFlight(req, res) {
   res.render('flights/new', {
     title: 'Add Flight',
   })
 }
 
+function create(req, res) {
+  // remove empty properties on req.body
+  // for (let number in req.body) {
+  //   if (req.body[number] === '') delete req.body[number]
+  // }
+  // use Flight model to create movie
+  Flight.create(req.body)
+  .then(flight => {
+    // redirect somewhere
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
 
 
 export {
   newFlight as new,
+  index,
+  create,
+  show,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,8 +3,18 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
+// GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
+
+
 // GET localhost:3000/flights/new
 router.get('/new', flightsCtrl.new)
+
+// GET localhost:3000/flights/:flightId
+router.get('/:flightId', flightsCtrl.show)
+
+// POST localhost:3000/flights
+router.post('/', flightsCtrl.create)
 
 
 
